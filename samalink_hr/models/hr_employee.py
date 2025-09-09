6e09927049663ac0f0caba4216b1c368368fb414
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2025-09-09 07:12:10 +0300

    [IMP] 'allow_check_from_odoo' field and update attendance check logic

--- samalink_hr/models/hr_employee.py
+++ samalink_hr/models/hr_employee.py
@@ -2,11 +2,13 @@ import logging
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
-_logger = logging.getLogger(__name__)
 
+_logger = logging.getLogger(__name__)
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
+
+    allow_check_from_odoo = fields.Boolean(string="Allow Check From Odoo", default=False)
 
     @api.constrains('pin')
     def _check_pin(self):
@@ -18,3 +20,9 @@ class HrEmployee(models.Model):
         for group in groups:
             if group['pin_count'] > 1:
                 raise UserError(f"PIN Code {group['pin']} must be unique found {group['pin_count']} instances.")
+
+    def _attendance_action_change(self, geo_information=None):
+        self.ensure_one()
+        if not self.allow_check_from_odoo:
+            raise UserError("You are not allowed to check in/out from Odoo. Please contact your administrator.")
+        return super()._attendance_action_change(geo_information=geo_information)
