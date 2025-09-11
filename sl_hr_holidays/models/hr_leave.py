fbbad62561bb9773a2f4fe6e46d0fbc5a1bd166a
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2025-09-11 07:40:01 +0300

    [IMP] Refactor leave request constraints and enhance leave type model with validation for requests limit and request offset

--- sl_hr_holidays/models/hr_leave.py
+++ sl_hr_holidays/models/hr_leave.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 class HrLeave(models.Model):
     _inherit = 'hr.leave'
 
-    @api.constrains('employee_id', 'request_date_from', 'request_date_to', 'holiday_status_id.requests_limit')
+    @api.constrains('employee_id', 'request_date_from', 'holiday_status_id')
     def _check_requests_limit(self):
         for record in self:
             if record.holiday_status_id.requests_limit > 0:
@@ -28,10 +28,11 @@ class HrLeave(models.Model):
                 if requests_count > record.holiday_status_id.requests_limit:
                     raise ValidationError(f"You have reached the maximum number of leave requests ({record.holiday_status_id.requests_limit}) for {record.holiday_status_id.name} in {record.request_date_from.strftime('%B %Y')}.")
 
-    @api.constrains('request_date_from', 'request_date_to', 'holiday_status_id.request_offset')
+    @api.constrains('request_date_from', 'holiday_status_id')
     def _check_request_offset(self):
         today = fields.Date.context_today(self)
-        for record in self:
+        requests = self.filtered(lambda r: r.holiday_status_id.enable_request_offset)
+        for record in requests:
             limit_date = record.request_date_from - timedelta(days=record.holiday_status_id.request_offset)
             if today > limit_date:
                 raise ValidationError(f"The leave request must be at least on or before {limit_date}.")
--- sl_hr_holidays/models/hr_leave_type.py
+++ sl_hr_holidays/models/hr_leave_type.py
@@ -1,8 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HrLeaveType(models.Model):
     _inherit = 'hr.leave.type'
 
     requests_limit = fields.Integer(string="Requests Limit (Monthly)", default=3, help="Maximum number of leave requests an employee can make in a month.")
-    request_offset = fields.Integer(string="Request Before (Days)", default=0, help="Number of days to offset the leave request.")
+    enable_request_offset = fields.Boolean(string="Notice Required", default=False, help="Enable request notice for leave requests.")
+    request_offset = fields.Integer(string="Notice Before (Days)", default=0, help="Number of days to before the leave request.")
+
+    @api.constrains('requests_limit', 'request_offset')
+    def _check_positive_values(self):
+        for record in self:
+            if record.requests_limit < 0:
+                raise ValidationError("Requests Limit (Monthly) must be a non-negative integer.")
+            if record.request_offset < 0:
+                raise ValidationError("Request Before (Days) must be a non-negative integer.")  
