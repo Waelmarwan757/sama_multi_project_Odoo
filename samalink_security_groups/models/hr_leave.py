7a504498c67d26e9fb3c3032f6b2b290be7530e5
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2025-09-17 07:07:25 +0300

    [ADD] Implement HR Attendance and Leave approval checks: add overtime approval logic in hr_attendance and enforce manager checks in hr_leave

--- samalink_security_groups/models/hr_leave.py
+++ samalink_security_groups/models/hr_leave.py
@@ -1,10 +1,18 @@
+import logging
 from odoo import models
+from odoo.exceptions import UserError
 
+_logger = logging.getLogger(__name__)
 
 class HrLeave(models.Model):
     _inherit = 'hr.leave'
 
     def action_approve(self):
+        is_sl_admin = self.env.user.has_group('samalink_security_groups.group_samalink_administrator')
+        current_employee_manager = self.sudo().employee_id.leave_manager_id
+        if is_sl_admin and self.env.user != current_employee_manager:
+            raise UserError("You cannot approve leaves for employees you do not manage.")
+
         is_sl_manager = self.env.user.has_group('samalink_security_groups.group_samalink_manager')
         timeoff_group = self.env.ref('hr_holidays.group_hr_holidays_user')
         if is_sl_manager:
