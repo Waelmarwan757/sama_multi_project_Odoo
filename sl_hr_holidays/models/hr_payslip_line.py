1f9875226a15c72d1ca2b989ca003ee7e72daecf
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2025-10-01 20:49:35 +0300

    [ADD] Enhance attendance tracking: include late check-in and early check-out fields in payslip view and update related records computation logic.

--- sl_hr_holidays/models/hr_payslip_line.py
+++ sl_hr_holidays/models/hr_payslip_line.py
@@ -9,6 +9,10 @@ class HrPayslipLine(models.Model):
     _inherit = 'hr.payslip.line'
 
     def _compute_related_records_count(self):
+        allowed_late_minutes = self.env['ir.config_parameter'].sudo().get_param('hr_attendance_deviation.allowed_late_minutes', default=30)
+        allowed_late_hours = int(allowed_late_minutes) / 60.0
+        allowed_early_leaving_minutes = self.env['ir.config_parameter'].sudo().get_param('hr_attendance_deviation.allowed_early_leaving_minutes', default=15)
+        allowed_early_leaving_hours = int(allowed_early_leaving_minutes) / 60.0
         for line in self:
             if line.salary_rule_id.code != 'LATE_PENALTY':
                 super(HrPayslipLine, line)._compute_related_records_count()
@@ -16,30 +20,35 @@ class HrPayslipLine(models.Model):
                 date_from = line.slip_id.date_from
                 date_to = line.slip_id.date_to
                 employee_id = line.slip_id.employee_id
-                leaves_count = self.env['hr.leave'].search_count([
-                    ('employee_id', '=', employee_id.id),
-                    ('request_date_from', '>=', date_from),
-                    ('request_date_to', '<=', date_to),
-                    ('state', '=', 'validate'),
-                    ('holiday_status_id.code', '=', 'LATE'),
+                domain = expression.AND([
+                    [('employee_id', '=', employee_id.id)],
+                    [('check_in', '>=', date_from), ('check_in', '<=', date_to)],
+                    ['|', ('late_check_in', '>', allowed_late_hours), ('early_check_out', '>', allowed_early_leaving_hours)],
                 ])
-                line.update({'related_records_count': leaves_count})
+                attendance_count = self.env['hr.attendance'].search_count(domain)
+                line.update({'related_records_count': attendance_count})
 
     def open_related_records(self):
         self.ensure_one()
+        allowed_late_minutes = self.env['ir.config_parameter'].sudo().get_param('hr_attendance_deviation.allowed_late_minutes', default=30)
+        allowed_late_hours = int(allowed_late_minutes) / 60.0
+        allowed_early_leaving_minutes = self.env['ir.config_parameter'].sudo().get_param('hr_attendance_deviation.allowed_early_leaving_minutes', default=15)
+        allowed_early_leaving_hours = int(allowed_early_leaving_minutes) / 60.0
         if self.salary_rule_id.code != 'LATE_PENALTY':
             return super(HrPayslipLine, self).open_related_records()
         else:
             date_from = self.slip_id.date_from
             date_to = self.slip_id.date_to
             employee_id = self.slip_id.employee_id
-            leave_dates_domain = self._get_leaves_dates_domain()
+            # leave_dates_domain = self._get_leaves_dates_domain()
             action = self.env["ir.actions.actions"]._for_xml_id(
                 "samalink_hr.action_hr_attendance_list_payslip"
             )
             action['domain'] = expression.AND([
                 [('employee_id', '=', employee_id.id)],
-                leave_dates_domain
+                [('check_in', '>=', date_from), ('check_in', '<=', date_to)],
+                ['|', ('late_check_in', '>', allowed_late_hours), ('early_check_out', '>', allowed_early_leaving_hours)],
+                # leave_dates_domain
             ])
             return action
 
