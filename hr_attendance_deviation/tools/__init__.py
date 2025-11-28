723a9c95d5b1c7eb32ecb829877669d991756a55
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2025-11-28 06:25:05 +0200

    [IMP] Fix float_to_time_obj method: clarify minutes calculation for improved readability.

--- hr_attendance_deviation/tools/__init__.py
+++ hr_attendance_deviation/tools/__init__.py
@@ -6,7 +6,7 @@ class Converter:
     @staticmethod
     def float_to_time_obj(float_time):
         hours = int(float_time // 1)
-        minutes = int(float_time % 1) * 60
+        minutes = int((float_time % 1) * 60)
         time_str = f"{hours:02}:{minutes:02}:00"
         return datetime.strptime(time_str, "%H:%M:%S").time()
 
